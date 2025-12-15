d6ef5c67418e832fcc993c4d13e71b916166dd6d
Sourabh861 <sourabhchoubey3490@gmail.com>
2025-12-15 13:08:42 +0530

    Content Based Recommender

---
+++ notebooks/data_cleaning.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+
+DATA_PATH = 'data/data/Music Info.csv'
+
+
+def clean_data(data):
+
+    return (
+        data
+        .drop_duplicates(subset='spotify_id')
+        .drop(columns=['genre','spotify_id'])
+        .fillna({'tags':'no_tags'})
+        .assign(
+            name=lambda x: x['name'].str.lower(),
+            artist=lambda x: x['artist'].str.lower(),
+            tags=lambda x: x['tags'].str.lower()
+        )
+        .reset_index(drop=True)
+    )
+
+def data_for_content_filtering(data):
+    return(
+        data
+        .drop(columns=['track_id','name','spotify_preview_url'])
+    )
+
+def main(data_path):
+
+    # load the data
+    data = pd.read_csv(data_path)
+
+    # perform data cleaning
+    cleaned_data = clean_data(data)
+
+    # save cleaned data
+    cleaned_data.to_csv('data/data/cleaned_data.csv',index=False)
+
+if __name__ == '__main__':
+    main(DATA_PATH)
