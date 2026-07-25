3d803feaa6eb51ca7998dfbfe4af6dd33da87dfd
Sourabh861 <sourabhchoubey3490@gmail.com>
2026-07-26 00:04:29 +0530

    Added Google Drive folder download

--- notebooks/app.py
+++ notebooks/app.py
@@ -4,39 +4,56 @@ from scipy.sparse import load_npz
 import pandas as pd
 from pathlib import Path
 import os
+import zipfile
 import gdown
 
 BASE_DIR = Path(__file__).resolve().parent
 
-FOLDER_URL = "https://drive.google.com/drive/folders/1OD1In1PqYsqGKwXDOZI3NlNd5BTpmhoZ?usp=sharing"
+# Google Drive File ID of data.zip
+FILE_ID = "1ikeYE3KvatvydZ6AC09rHbciZofKNRsm"
 
-if not (BASE_DIR/"data").exists():
-    gdown.download_folder(
-        url=FOLDER_URL,
-        output=str(BASE_DIR),
-        quiet=False,
-        remaining_ok=True
+ZIP_PATH = BASE_DIR / "data.zip"
+
+# Download and extract only if the data folder doesn't exist
+if not (BASE_DIR / "data").exists():
+
+    gdown.download(
+        f"https://drive.google.com/uc?id={FILE_ID}",
+        str(ZIP_PATH),
+        quiet=False
     )
+
+    with zipfile.ZipFile(ZIP_PATH, "r") as zip_ref:
+        zip_ref.extractall(BASE_DIR)
 
 print("Current directory:", BASE_DIR)
 print("Files:", os.listdir(BASE_DIR))
 
-# Check both possible folder structures
+# Detect folder structure automatically
 if (BASE_DIR / "data" / "cleaned_data.csv").exists():
     cleaned_data_path = BASE_DIR / "data" / "cleaned_data.csv"
     transformed_data_path = BASE_DIR / "data" / "transformed_data.npz"
+
 elif (BASE_DIR / "data" / "data" / "cleaned_data.csv").exists():
     cleaned_data_path = BASE_DIR / "data" / "data" / "cleaned_data.csv"
     transformed_data_path = BASE_DIR / "data" / "data" / "transformed_data.npz"
-else:
-    raise FileNotFoundError("Data folder was not downloaded correctly.")
 
-# load the data
+else:
+    raise FileNotFoundError(
+        f"""
+Data folder was not extracted correctly.
+
+BASE_DIR = {BASE_DIR}
+
+Folders = {os.listdir(BASE_DIR)}
+"""
+    )
+
+# Load data
 data = pd.read_csv(cleaned_data_path)
 
-# load the transformed data
+# Load transformed data
 transformed_data = load_npz(transformed_data_path)
-
 # Title
 st.title('Welcome to the Spotify Song Recommender!')
 
